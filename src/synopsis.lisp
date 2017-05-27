4b0f86035088b1d40b4062688f3f10308597309c
miacro <fqguozhou@gmail.com>
2017-05-27 18:23:15 +0800

    ....

--- src/synopsis.lisp
+++ src/synopsis.lisp
@@ -1,3 +1,12 @@
 (in-package :command-line-options)
 (defun command-line ()
-  #+sbcl sb-ext:*posix-argv*)
+  #+abcl      ext:*command-line-argument-list*
+  #+allegro   sys:command-line-arguments
+  #+:ccl      ccl:*command-line-argument-list*
+  #+clisp     (cons *load-truename* ext:*args*)
+  #+clozure   ccl::command-line-arguments
+  #+cmu       extensions:*command-line-words*
+  #+ecl       (ext:command-args)
+  #+gcl       si:*command-args*
+  #+lispworks system:*line-arguments-list*
+  #+sbcl      sb-ext:*posix-argv*)
